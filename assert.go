19f1b2c52f232f861da06fa209c9b5a061d735aa
luolijun <libbylg@126.com>
2017-05-02 15:12:17 +0800

    解决静态质量检查工具发现的问题

--- assert.go
+++ assert.go
@@ -1,9 +1,9 @@
 package assert
 
 import (
-	"testing"
 	"reflect"
 	"runtime"
+	"testing"
 )
 
 type Assert struct {
@@ -15,7 +15,7 @@ func New(t *testing.T) *Assert {
 	return &a
 }
 
-func (a*Assert) Equal(message string, exp, got interface{}) {
+func (a *Assert) Equal(message string, exp, got interface{}) {
 	result := reflect.DeepEqual(exp, got)
 	if !result {
 		_, file, line, _ := runtime.Caller(1)
@@ -24,7 +24,7 @@ func (a*Assert) Equal(message string, exp, got interface{}) {
 	}
 }
 
-func (a*Assert) NotEqual(message string, exp, got interface{}) {
+func (a *Assert) NotEqual(message string, exp, got interface{}) {
 	result := reflect.DeepEqual(exp, got)
 	if result {
 		_, file, line, _ := runtime.Caller(1)
@@ -33,7 +33,7 @@ func (a*Assert) NotEqual(message string, exp, got interface{}) {
 	}
 }
 
-func (a*Assert) True(message string, got bool) {
+func (a *Assert) True(message string, got bool) {
 	result := reflect.DeepEqual(true, got)
 	if !result {
 		_, file, line, _ := runtime.Caller(1)
@@ -42,7 +42,7 @@ func (a*Assert) True(message string, got bool) {
 	}
 }
 
-func (a*Assert) False(message string, got bool) {
+func (a *Assert) False(message string, got bool) {
 	result := reflect.DeepEqual(false, got)
 	if !result {
 		_, file, line, _ := runtime.Caller(1)
@@ -51,25 +51,25 @@ func (a*Assert) False(message string, got bool) {
 	}
 }
 
-func (a*Assert) Panic(message string, fn func()) {
+func (a *Assert) Panic(message string, fn func()) {
 	defer func() {
 		recover()
 	}()
-	
+
 	fn()
-	
+
 	_, file, line, _ := runtime.Caller(1)
 	a.T.Errorf("\n%s:%d\n%s:\n\t- Expect panic:%s\n\t- Expect:Panic\n\t- ButGot:No Panic\n", file, line, a.T.Name(), message)
 	a.T.FailNow()
 }
 
-func (a*Assert) NoPanic(message string, fn func()) {
+func (a *Assert) NoPanic(message string, fn func()) {
 	defer func() {
 		r := recover()
 		_, file, line, _ := runtime.Caller(3)
 		a.T.Errorf("\n%s:%d\n%s:\n\t- Expect no panic:%s\n\t- Expect:No Panic\n\t- ButGot:Panic(%v)\n", file, line, a.T.Name(), message, r)
 		a.T.FailNow()
 	}()
-	
+
 	fn()
 }
